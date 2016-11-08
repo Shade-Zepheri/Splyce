2cfc7af7f2f2a417c6fa30c9148053e5e88b4015
Shade-Zepheri <ziroalpha@gmail.com>
2016-11-07 21:54:16 -0600

    Commits for Tonight

--- .theos/obj/debug/armv7/Tweak.xm.mm
+++ .theos/obj/debug/armv7/Tweak.xm.mm
@@ -1,8 +1,34 @@
+#line 1 "Tweak.xm"
 #import "Interfaces.h"
 
 @interface Splyce : NSObject <LAListener>
 @end
 
+
+#include <substrate.h>
+#if defined(__clang__)
+#if __has_feature(objc_arc)
+#define _LOGOS_SELF_TYPE_NORMAL __unsafe_unretained
+#define _LOGOS_SELF_TYPE_INIT __attribute__((ns_consumed))
+#define _LOGOS_SELF_CONST const
+#define _LOGOS_RETURN_RETAINED __attribute__((ns_returns_retained))
+#else
+#define _LOGOS_SELF_TYPE_NORMAL
+#define _LOGOS_SELF_TYPE_INIT
+#define _LOGOS_SELF_CONST
+#define _LOGOS_RETURN_RETAINED
+#endif
+#else
+#define _LOGOS_SELF_TYPE_NORMAL
+#define _LOGOS_SELF_TYPE_INIT
+#define _LOGOS_SELF_CONST
+#define _LOGOS_RETURN_RETAINED
+#endif
+
+@class SBUIController; @class FBProcessManager; @class SBAppSwitcherModel; 
+
+static __inline__ __attribute__((always_inline)) __attribute__((unused)) Class _logos_static_class_lookup$SBUIController(void) { static Class _klass; if(!_klass) { _klass = objc_getClass("SBUIController"); } return _klass; }static __inline__ __attribute__((always_inline)) __attribute__((unused)) Class _logos_static_class_lookup$FBProcessManager(void) { static Class _klass; if(!_klass) { _klass = objc_getClass("FBProcessManager"); } return _klass; }static __inline__ __attribute__((always_inline)) __attribute__((unused)) Class _logos_static_class_lookup$SBAppSwitcherModel(void) { static Class _klass; if(!_klass) { _klass = objc_getClass("SBAppSwitcherModel"); } return _klass; }
+#line 6 "Tweak.xm"
 @implementation Splyce
 
 + (void)load{
@@ -24,8 +50,7 @@
 	event.handled = YES;
 	dispatch_async(dispatch_get_main_queue(), ^{
 		CFPreferencesAppSynchronize(CFSTR("com.shade.splyce"));
-		// UnFinished Clear Switcher 
-		/* SBAppSwitcherModel *model = (SBAppSwitcherModel *)[%c(SBAppSwitcherModel) sharedInstance];
+		SBAppSwitcherModel *model = (SBAppSwitcherModel *)[_logos_static_class_lookup$SBAppSwitcherModel() sharedInstance];
 		NSArray *identifiers = [model respondsToSelector:@selector(identifiers)] ? model.identifiers : model.snapshot;
 		if ([identifiers count]) {
 			Boolean exists = false;
@@ -40,10 +65,10 @@
 				}
 			}
 		}
-		SBUIController *uic = (SBUIController *)[%c(SBUIController) sharedInstance];
-		[uic dismissSwitcherAnimated:YES]; */
+		SBUIController *uic = (SBUIController *)[_logos_static_class_lookup$SBUIController() sharedInstance];
+		[uic dismissSwitcherAnimated:YES];
 		FBApplicationProcess *currentProcess = [(SpringBoard*)[UIApplication sharedApplication] _accessibilityFrontMostApplication];
-		for (FBApplicationProcess *process in [(FBProcessManager *)[%c(FBProcessManager) sharedInstance] allApplicationProcesses]) {
+		for (FBApplicationProcess *process in [(FBProcessManager *)[_logos_static_class_lookup$FBProcessManager() sharedInstance] allApplicationProcesses]) {
 			if (!process.nowPlayingWithAudio && !process.recordingAudio && (process != currentProcess)) {
 				BKSProcess *bkProcess = MSHookIvar<BKSProcess*>(process, "_bksProcess");
 				if (bkProcess) {
@@ -55,3 +80,4 @@
 }
 
 @end
+#line 57 "Tweak.xm"
